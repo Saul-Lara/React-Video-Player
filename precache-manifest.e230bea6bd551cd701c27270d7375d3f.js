966e21acbf67c3579f0bb2cb116978a6c59f7bde
Saul-Lara <33563111+Saul-Lara@users.noreply.github.com>
2019-10-09 06:21:05 -0500

    Updates

---
+++ precache-manifest.e230bea6bd551cd701c27270d7375d3f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60fb0e41d809132d7f80dd3b952bfb0e",
+    "url": "/React-Video-Player/index.html"
+  },
+  {
+    "revision": "179aa5409120a0e590eb",
+    "url": "/React-Video-Player/static/js/2.985deec5.chunk.js"
+  },
+  {
+    "revision": "60c06101105d28e64071",
+    "url": "/React-Video-Player/static/js/main.9cab51cc.chunk.js"
+  },
+  {
+    "revision": "5e845fb20fd941c922ff",
+    "url": "/React-Video-Player/static/js/runtime~main.1306688a.js"
+  }
+]);
